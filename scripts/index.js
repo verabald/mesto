2b969bdcdb6bc5a1f7d033557e70f8e671245da1
Vera Baldina~ <vera.b97@mail.ru>
2023-03-06 21:28:28 +0300

    feat: изменения профиля пользователя

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const popup = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonClose = popup.querySelector('.popup__button-close');
+const buttonSave = popup.querySelector('.popup__button-submit');
 
 const openPopup = function () {
     popup.classList.add ('popup_opened');
@@ -20,3 +21,23 @@ const closePopupByOverlay = function (event) {
 buttonEdit.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
 popup.addEventListener('click', closePopupByOverlay);
+buttonSave.addEventListener('click', closePopup);
+
+const popupElement = popup.querySelector('.popup__container');
+const nameInput = popupElement.querySelector('.popup__input-name');
+const jobInput = popupElement.querySelector('.popup__input-job');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+
+    nameInput.getAttribute ('value');
+    jobInput.getAttribute ('value');
+
+    const nameElement = document.querySelector('.profile__name');
+    const jobElement = document.querySelector('.profile__profession');
+
+    nameElement.textContent = nameInput.value;
+    jobElement.textContent = jobInput.value;
+};
+
+popupElement.addEventListener('submit', handleFormSubmit); 
