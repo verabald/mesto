cc9abac7373ff5492a8d6ebe7ccee09fdf441289
Vera Baldina~ <vera.b97@mail.ru>
2023-03-20 21:40:54 +0300

    build: добавить карточки через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,48 @@
+const cards = [
+    {
+        title: 'Карачаевск',
+        link: './images/elements-karachaevsk.jpg',
+    },
+
+    {
+        title: 'Озеро Гижгит',
+        link: './images/elements-gizgit.jpg',
+    },
+
+    {
+        title: 'Гора Домбай',
+        link: './images/elements-dombay.jpg',
+    },
+
+    {
+        title: 'Ущелье Адыл-су',
+        link: './images/elements-adil_su.jpg',
+    },
+
+    {
+        title: 'Урочище Джилы-Су',
+        link: './images/elements-jili_su.jpg',
+    },
+
+    {
+        title: 'Карачаево-Черкесия',
+        link: './images/elements-karachaevo.jpg',
+    }
+];
+
+const cardTemplate = document.querySelector('.elements__template').content;
+const cardsList = document.querySelector('.elements__list');
+
+cards.forEach(createCard);
+
+function createCard (item) {
+    const cardElement = cardTemplate.cloneNode(true);
+	cardElement.querySelector('.elements__caption').textContent = item.title;
+    cardElement.querySelector('.elements__image').src = item.link;
+
+	cardsList.append(cardElement);
+};
+
 const popup = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonAdd = document.querySelector('.profile__add-button');
