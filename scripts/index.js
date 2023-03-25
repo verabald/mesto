2322f38c12b88c104f5abcacabfef8578a66e30e
Vera Baldina~ <vera.b97@mail.ru>
2023-03-25 22:33:36 +0300

    refactor: вынести карточки отдельным скриптом

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,3 @@
-const cards = [
-    {
-        title: 'Карачаевск',
-        link: './images/elements-karachaevsk.jpg',
-    },
-
-    {
-        title: 'Озеро Гижгит',
-        link: './images/elements-gizgit.jpg'
-    },
-
-    {
-        title: 'Гора Домбай',
-        link: './images/elements-dombay.jpg'
-    },
-
-    {
-        title: 'Ущелье Адыл-су',
-        link: './images/elements-adil_su.jpg'
-    },
-
-    {
-        title: 'Урочище Джилы-Су',
-        link: './images/elements-jili_su.jpg'
-    },
-
-    {
-        title: 'Карачаево-Черкесия',
-        link: './images/elements-karachaevo.jpg'
-    }
-];
-
 const cardTemplate = document.querySelector('.elements__template').content;
 const cardsContainer = document.querySelector('.elements__list');
 
