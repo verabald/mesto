9362a99b1d72aa2be9067d5c22f2e1c2ee9bb46c
Vera Baldina~ <vera.b97@mail.ru>
2023-04-01 21:59:13 +0300

    refactor: убрать лишние пробелы

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ const buttonZoom = document.querySelector('.elements__image');
 const captionPopup = document.querySelector('.popup__caption');
 const imagePopup = document.querySelector('.popup__image');
 
-function createCard (item) {
+function createCard(item) {
     const cardElement = cardTemplate.cloneNode(true);
     cardElement.querySelector('.elements__image').src = item.link;
 	cardElement.querySelector('.elements__caption').textContent = item.title;
@@ -17,7 +17,7 @@ function createCard (item) {
         imagePopup.src = item.link;
         captionPopup.textContent = item.title;
         imagePopup.alt = item.title;
-        openPopup (popupZoom);
+        openPopup(popupZoom);
   });
   
     cardElement.querySelector('.elements__like-button').addEventListener('click', function (evt) {
@@ -33,7 +33,7 @@ function createCard (item) {
 
 cards.reverse().forEach(renderCard);
 
-function renderCard (item) {
+function renderCard(item) {
     cardsContainer.prepend(createCard(item));
 };
 
@@ -43,7 +43,7 @@ const popupAdd = document.querySelector('.popup_mode_add');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const buttonAdd = document.querySelector('.profile__add-button');
 
-function openPopup (popup) {
+function openPopup(popup) {
     popup.classList.add ('popup_opened');
 };
 
@@ -58,11 +58,11 @@ buttonAdd.addEventListener('click', () => {
     linkInput.value = '';
 });
 
-function closePopup (popup) {
+function closePopup(popup) {
     popup.classList.remove ('popup_opened');
 };
 
-function closePopupByOverlay  (evt) { 
+function closePopupByOverlay(evt) { 
     if (evt.target !== evt.currentTarget) { 
         return; 
     } 
@@ -72,7 +72,7 @@ function closePopupByOverlay  (evt) {
 popupAdd.addEventListener('click', closePopupByOverlay);
 popupEdit.addEventListener('click', closePopupByOverlay);
 
-function closePopupByEsc (evt) {
+function closePopupByEsc(evt) {
     if (evt.key === 'Escape') {
         const popupList = Array.from(document.querySelectorAll('.popup'));
         popupList.forEach((popupElement) => {
@@ -95,7 +95,7 @@ const jobElement = document.querySelector('.profile__profession');
 const nameInput = popupFormEditing.querySelector('.popup__input_field_name');
 const jobInput = popupFormEditing.querySelector('.popup__input_field_job');
 
-function saveFormEdit (evt) {
+function saveFormEdit(evt) {
     evt.preventDefault(); 
 
     nameElement.textContent = nameInput.value;
@@ -111,7 +111,7 @@ const popupFormAdding = popupAdd.querySelector('.popup__form');
 const titleInput = popupFormAdding.querySelector('.popup__input_field_title');
 const linkInput = popupFormAdding.querySelector('.popup__input_field_link');
 
-function saveFormCard (evt) {
+function saveFormCard(evt) {
     evt.preventDefault(); 
 
     renderCard({title: titleInput.value, link: linkInput.value});
